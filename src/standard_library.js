47e9b48ed692f59770230f8d5b01a4a7887eb082
Adam <hutty.jpnm@hotmail.com>
2024-06-22 23:56:32 +0000

    Add initial standard library functions

---
+++ src/standard_library.js
@@ -0,0 +1,9 @@
+// standard_library.js
+
+const stringLength = (s) => s.length;
+const stringConcat = (s1, s2) => s1 + s2;
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+
+module.exports = { stringLength, stringConcat, add, subtract };
+
