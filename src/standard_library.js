39b46f37ee39950f03669194cef44fce599b036c
Adam <hutty.jpnm@hotmail.com>
2024-06-23 00:07:52 +0000

    Expand standard library with multiply and divide functions

--- src/standard_library.js
+++ src/standard_library.js
@@ -5,5 +5,9 @@ const stringConcat = (s1, s2) => s1 + s2;
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 
-module.exports = { stringLength, stringConcat, add, subtract };
+// New functions
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+
+module.exports = { stringLength, stringConcat, add, subtract, multiply, divide };
 
